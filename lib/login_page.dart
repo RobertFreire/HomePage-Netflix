224bc543935364327c7efdd96b857720affbfda5
Robert <robertfreire236@gmail.com>
2023-06-16 17:22:23 -0300

    Login Page atualizada

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:async';
 
 import 'package:flutter/material.dart';
 
@@ -14,73 +13,137 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    String email = "";
-    String senha = "";
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              TextField(
-                onChanged: (value) => email = value,
-                decoration: const InputDecoration(
-                    labelText: 'E-mail',
-                    border: OutlineInputBorder(),
-                    hintText: 'example@gmail.com'),
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                onChanged: (value) => senha = value,
-                obscureText: true,
-                decoration: const InputDecoration(
-                    labelText: 'Senha', border: OutlineInputBorder()),
-              ),
-              const SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        if (email == 'robert@gmail.com' && senha == '1234') {
-                          Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                  builder: (context) => const HomePage()));
-                        } else {
-                          DialogBuilder(context);
-                        }
-                      },
-                      child: const Text('Acessar')),
-                  const SizedBox(width: 5),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const CadastroPage()));
-                      },
-                      child: const Text('Cadastrar'))
-                ],
-              ),
-            ]),
+  String email = "";
+  String senha = "";
+
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, top: 5, right: 20, bottom: 200),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      height: 300,
+                      child: Image.asset(
+                        'assets/netflix.png',
+                      ),
+                    ),
+                    Card(
+                      color: Colors.black.withOpacity(0.5),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          children: [
+                            TextField(
+                              onChanged: (value) => email = value,
+                              decoration: const InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  labelText: 'E-mail',
+                                  border: OutlineInputBorder(),
+                                  hintText: 'example@gmail.com'),
+                            ),
+                            const SizedBox(height: 10),
+                            TextField(
+                              onChanged: (value) => senha = value,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  labelText: 'Senha',
+                                  border: OutlineInputBorder()),
+                            ),
+                            const SizedBox(height: 15),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
+                                    onPressed: () {
+                                      if (email == 'robert@gmail.com' &&
+                                          senha == '1234') {
+                                        Navigator.of(context).pushReplacement(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const HomePage()));
+                                      } else {
+                                        _dialogBuilder(context);
+                                      }
+                                    },
+                                    child: const SizedBox(
+                                        width: double.infinity,
+                                        child: Text(
+                                          'Acessar',
+                                          textAlign: TextAlign.center,
+                                        ))),
+                                const SizedBox(height: 15),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const CadastroPage()));
+                                  },
+                                  child: const SizedBox(
+                                    width: double.infinity,
+                                    child: Text(
+                                      'Cadastrar',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
           ),
         ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.6),
+          ),
+          _body(),
+        ],
       ),
     );
   }
 
-  Future<void> DialogBuilder(BuildContext context) {
+  Future<void> _dialogBuilder(BuildContext context) {
     return showDialog(
         context: context,
         builder: (BuildContext context) => AlertDialog(
-              title: const Text('Cadastro Invalido'),
+              title: const Text('Acesso Invalido'),
               actions: <Widget>[
                 TextButton(
                     onPressed: () => Navigator.pop(context, 'OK'),
-                    child: Text('OK'))
+                    child: const Text('OK'))
               ],
             ));
   }
